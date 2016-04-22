ca6af6d266924c96b6527521a84b0cac448edab4
Jari Jaanto <jaffa@jaffa.xyz>
2016-04-22 23:38:23 +0300

    two

--- www/js/eoi.js
+++ www/js/eoi.js
@@ -1,21 +1,26 @@
 $( document ).ready(function() {
     var len = 100;
-    dataArr = [];
+    dataArr0 = [];
+    dataArr1 = [];
     for (var i = 0; i < len; i++) {
-        dataArr.push(0);
+        dataArr0.push(0);
+        dataArr1.push(0);
     }
+
     var socket = io('http://ehd2016.azurewebsites.net');
     console.log(socket);
     socket.on('connection', function() {
         socket.emit('subscribe', 'muse0');
+        socket.emit('subscribe', 'muse1');
     })
     socket.on('data', function (data) {
         if(data.room === 'muse0') {
-            //console.log("DAT");
-            dataArr.push(parseInt(data.data[0]*100));
-            dataArr.shift();
-            //console.log(dataArr);
-//        document.getElementById('log').innerHTML = JSON.stringify(data);
+            dataArr0.push(parseInt(data.data[0]*100+300));
+            dataArr0.shift();
+        }
+        if(data.room === 'muse1') {
+            dataArr1.push(parseInt(data.data[0]*100+300));
+            dataArr1.shift();
         }
     });
 //document.getElementById('log').innerHTML();
@@ -26,14 +31,23 @@ $( document ).ready(function() {
         var ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+        ctx.strokeStyle = "#FF0000";
         ctx.moveTo(0,0);
         ctx.beginPath();
         for (var i = 0; i < len; i++) {
             //console.log(dataArr[i]);
-            ctx.lineTo(i*10, dataArr[i]);
+            ctx.lineTo(i*10, dataArr0[i]);
         }
         ctx.stroke();
-        setTimeout(line, 200);
+
+        ctx.strokeStyle = "#0000FF";
+        ctx.moveTo(0,0);
+        ctx.beginPath();
+        for (var i = 0; i < len; i++) {
+            ctx.lineTo(i*10, dataArr1[i]);
+        }
+        ctx.stroke();
+        setTimeout(line, 100);
         //console.log("line-end");
     }
 
